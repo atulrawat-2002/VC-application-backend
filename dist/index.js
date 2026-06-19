feee4545154141683bf83cc5d3bf53792ace1f81
Atul <dr6217811@gmail.com>
2026-06-19 08:59:47 +0530

    setup the basic express and io server

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,24 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Hello Universe");
+import cors from "cors";
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import ServerConfig from "./config/serverConfig.js";
+const app = express();
+const server = http.createServer(app);
+app.use(cors());
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "post"],
+    },
+});
+io.on("connection", (socket) => {
+    console.log("A connection established");
+    socket.on("disconnected", () => {
+        console.log("Socket disconnected");
+    });
+});
+server.listen(ServerConfig.PORT, () => {
+    console.log(`Server is up at port ${ServerConfig.PORT}`);
+});
 //# sourceMappingURL=index.js.map
