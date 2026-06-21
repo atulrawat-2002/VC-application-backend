db534eb5484268446a5e24d34745485b61b65e1b
Atul <dr6217811@gmail.com>
2026-06-21 11:00:38 +0530

    Added user feed on frontend

--- src/handlers/roomHandler.ts
+++ src/handlers/roomHandler.ts
@@ -1,17 +1,30 @@
 import { Socket } from "socket.io";
 import { v4 as UUIDV4 } from "uuid";
+import type { IRoomParams } from "../interfaces/IRoomParams.js";
+
+const rooms: Record<string, string[]> = {};
 
 const roomHandler = (socket: Socket) => {
 
     const createRoom = () => {
         const roomId = UUIDV4();
         socket.join(roomId);
+        rooms[roomId] = [];
+
         socket.emit("room-created", { roomId });
         console.log("A new room has been creating room with id", roomId);
     };
 
-    const joinedRoom = ({roomId}: {roomId: string}) => {
-        console.log("A new user has joined the room with id", roomId);
+    const joinedRoom = ({roomId, peerId}: IRoomParams) => {
+        if (rooms[roomId]) {
+            console.log("A new user has joined the room with id", roomId, " with peerId ", peerId);
+            rooms[roomId]?.push(peerId);
+            socket.join(roomId);
+        }
+        socket.emit("get-users", {
+            roomId,
+            participants: rooms[roomId],
+        });
     };
 
     socket.on("create-room", createRoom);
